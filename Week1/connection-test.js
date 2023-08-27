89c9a47bca92a1bee6c9092a98f1ac88e129fd44
odrinl <keozon@gmail.com>
2023-08-27 03:34:55 +0200

    bug

--- Week1/connection-test.js
+++ Week1/connection-test.js
@@ -1,7 +1,6 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
-  port: 3306,
   user     : 'hyfuser',
   password : 'hyfpassword',
   database : 'userdb',
@@ -10,7 +9,8 @@ var connection = mysql.createConnection({
   // code: 'ECONNREFUSED',
   // Then, the following line will solve it
   // Note that your socket file path may be different
-  // socketPath: '/tmp/ mysqladmin -p -u <user-name> variables'
+  socketPath: '/tmp/mysql.sock'
+  
   // port : xxxx // Uncomment this line and replace xxxx with the selected port number if you are not using default 3306. I also suggest to download MySQL version 5.7 because recent versions has authentication problems
 
 });
