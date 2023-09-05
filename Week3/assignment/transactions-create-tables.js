1f83bb1e79e4b08119cb1849dca58fce60a3461f
Audrey <keozon@gmail.com>
2023-09-05 23:40:26 +0200

    ex2

---
+++ Week3/assignment/transactions-create-tables.js
@@ -0,0 +1,65 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'hyfuser',
+  password: 'hyfpassword',
+});
+
+const databaseName = 'transactions';  
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+// Create the database (replace 'your_database_name' with your desired database name)
+db.query(`CREATE DATABASE IF NOT EXISTS ${databaseName}`, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Database "${databaseName}" created or already exists.`);
+});
+
+// Use the created database
+db.query(`USE ${databaseName}`, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  console.log(`Using database "${databaseName}".`);
+});
+
+// Create the account table
+db.query(`
+  CREATE TABLE IF NOT EXISTS account (
+    account_number INT PRIMARY KEY,
+    balance DECIMAL(10, 2) NOT NULL
+  )
+`);
+
+// Create the account_changes table
+db.query(`
+  CREATE TABLE IF NOT EXISTS account_changes (
+    change_number INT AUTO_INCREMENT PRIMARY KEY,
+    account_number IN NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    changed_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    remark TEXT,
+    FOREIGN KEY (account_number) REFERENCES account(account_number)
+  )
+`);
+
+// Close the database connection
+connection.end((err) => {
+  if (err) {
+    console.error('Error closing the database connection: ' + err.stack);
+    return;
+  }
+  console.log('Closed the database connection');
+});
---
+++ Week3/assignment/transactions-insert-values.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+
+// Create a connection to the MySQL database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'hyfuser',
+  password: 'hyfpassword',
+  database: 'transactions', 
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database: ' + err.stack);
+    return;
+  }
+  console.log('Connected to the database');
+});
+
+// Insert sample data into the account table
+db.query('INSERT INTO account (account_number, balance) VALUES (12345, 5000)');
+db.query('INSERT INTO account (account_number, balance) VALUES (12346, 3000)');
+
+// Close the database connection
+connection.end((err) => {
+  if (err) {
+    console.error('Error closing the database connection: ' + err.stack);
+    return;
+  }
+  console.log('Closed the database connection');
+});
